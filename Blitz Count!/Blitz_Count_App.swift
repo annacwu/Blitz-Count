6049fb88a44fef022a02732f1a5fca527cc963a0
annacwu <annacwu04@gmail.com>
2024-03-19 12:30:39 -0700

    Initial Commit

---
+++ Blitz Count!/Blitz_Count_App.swift
@@ -0,0 +1,17 @@
+//
+//  Blitz_Count_App.swift
+//  Blitz Count!
+//
+//  Created by Anna Wu on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Blitz_Count_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
