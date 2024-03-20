6049f968cee8869c2c2d7bff951b90e334d1064a
annacwu <annacwu04@gmail.com>
2024-03-20 10:44:46 -0700

    Add AddPlayerView

---
+++ Blitz Count!/ViewModels/AddPlayerViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AddPlayerViewViewModel.swift
+//  Blitz Count!
+//
+//  Created by Anna Wu on 3/20/24.
+//
+
+import Foundation
---
+++ Blitz Count!/Views/AddPlayerView.swift
@@ -0,0 +1,18 @@
+//
+//  AddPlayerView.swift
+//  Blitz Count!
+//
+//  Created by Anna Wu on 3/20/24.
+//
+
+import SwiftUI
+
+struct AddPlayerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPlayerView()
+}
