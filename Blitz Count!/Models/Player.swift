f87b413273eac171fe622046cfa937fedb97d220
annacwu <annacwu04@gmail.com>
2024-03-21 10:43:11 -0700

    Add Player class

--- Blitz Count!/Models/Player.swift
+++ Blitz Count!/Models/Player.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Player {
+    var name: String
+    var currPoints: Int
+    var wins: Int
+    var totalPoints: Int
+    var gamesPlayed: Int
+    
+    init(name: String, currPoints: Int, wins: Int, totalPoints: Int, gamesPlayed: Int) {
+        self.name = name
+        self.currPoints = currPoints
+        self.wins = wins
+        self.totalPoints = totalPoints
+        self.gamesPlayed = gamesPlayed
+    }
+}
--- Blitz Count!/Other/BlitzCountApp.swift
+++ Blitz Count!/Other/BlitzCountApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BlitzCountApp: App {
@@ -13,5 +14,6 @@ struct BlitzCountApp: App {
         WindowGroup {
             GameView()
         }
+        .modelContainer(for: Player.self)
     }
 }
